ee0d5eb944eddda2b1f2cecde87520ace392218d
ANooice-ckpt <yuxiaowang.pt@gmail.com>
2026-08-20 22:47:33 +0800

    Initialize tidyverse on RQ1 context PSOCK workers

--- scripts/10c_rq1_context_finalize.R
+++ scripts/10c_rq1_context_finalize.R
@@ -143,6 +143,9 @@ parallel_bootstrap <- function(tasks, workers = CONTEXT_WORKERS) {
       OMP_NUM_THREADS = "1", OPENBLAS_NUM_THREADS = "1", MKL_NUM_THREADS = "1",
       VECLIB_MAXIMUM_THREADS = "1", NUMEXPR_NUM_THREADS = "1"
     )
+    suppressPackageStartupMessages({
+      library(tidyverse)
+    })
     NULL
   }, root)
   parallel::clusterExport(
