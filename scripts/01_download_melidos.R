1bae9c33ddac88c23d9bd85afda187c25590dfff
ANooice-ckpt <yuxiaowang.pt@gmail.com>
2026-08-25 14:32:33 +0800

    Download exercise diaries for layered RQ2 context

--- scripts/01_download_melidos.R
+++ scripts/01_download_melidos.R
@@ -4,7 +4,7 @@ options(timeout = max(3600, getOption("timeout")))
 sites <- melidos_sites()
 modalities <- c(
   "light_glasses", "light_chest", "light_wrist",
-  "wearlog", "sleepdiaries", "lightexposurediary", "trial_times"
+  "wearlog", "sleepdiaries", "lightexposurediary", "exercisediary", "trial_times"
 )
 optional_modalities <- "trial_times"
 site_override <- Sys.getenv("MELIDOS_SITES", "")
